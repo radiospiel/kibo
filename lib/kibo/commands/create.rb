a93369e9e95a10dd9f0f6cc311f05381ade0efc0
radiospiel <eno@open-lab.org>
2012-10-02 15:36:51 +0200

    Adds remotes and collaborate subcommands

--- lib/kibo/commands/create.rb
+++ lib/kibo/commands/create.rb
@@ -1,13 +1,43 @@
 module Kibo::Commands
-  subcommand :create, "create missing targets" do
-    opt :all, "Create all missing targets.", :short => "a"
+  subcommand :remotes, "create missing remotes" do
+    opt :all, "Create all missing remotes.", :short => "a"
   end
+
+  subcommand :create, "create missing apps"
+  
+  subcommand :collaborate, "add collaborations"
 
   def create
     verify_heroku_login
 
+    apps = heroku("apps").split("\n").reject { |line| line =~ /===/ }
+    missing_apps = h.expected_remotes - apps
+    
+    confirm! <<-MSG
+I am going to create these instances: #{missing_apps.map(&:inspect).join(", ")} for you [#{Kibo.config.account}].
+MSG
+
+    missing_apps.each do |instance|
+      create_instance(instance)
+      Kibo.config.collaborations.each do |email|
+        heroku "sharing:add", email 
+      end
+    end
+  end
+
+  def collaborate
+    verify_heroku_login
+    
+    h.expected_remotes.each do |instance|
+      Kibo.config.collaborations.each do |email|
+        heroku "sharing:add", email, "--app", instance
+      end
+    end
+  end
+  
+  def remotes
     if Kibo.command_line.all? 
-      instances = missing_remotes
+      instances = h.missing_remotes
       if instances.empty?
         W "Nothing to do."
         exit 0
@@ -20,7 +50,7 @@ module Kibo::Commands
       end
 
       # only create instances that are actually missing.
-      extra_instances = instances - missing_remotes
+      extra_instances = instances - h.missing_remotes
       unless extra_instances.empty?
         E <<-MSG
 kibo cannot create these instances for you: #{extra_instances.map(&:inspect).join(", ")}, because I don't not know anything about these.
@@ -28,13 +58,8 @@ MSG
       end
     end
 
-    confirm! <<-MSG
-I am going to create these instances: #{instances.map(&:inspect).join(", ")}. Is this what you want? Note:
-You are logged in at heroku as #{config.account}.
-MSG
-
-    instances.each do |instance|
-      create_instance(instance)
+    h.missing_remotes.each do |remote|
+      git "remote", "add", remote, "git@heroku.com:#{remote}.git"
     end
   end
   
@@ -50,7 +75,7 @@ MSG
     if !whoami
       E "Please log in ('heroku auth:login') as #{config.account}."
     elsif whoami != Kibo.config.account
-      E "You are currently logged in as #{whoami}; please log in ('heroku auth:login') as #{config.account}."
+      E "You are currently logged in as #{whoami}; please log in ('heroku auth:login') as #{Kibo.config.account}."
     end
   end
 end
