8a48de104d3fbab00f1d5fb5c2517b53617f7749
radiospiel <eno@open-lab.org>
2012-10-02 15:37:09 +0200

    "info" subcommand includes process info

--- lib/kibo/commands/info.rb
+++ lib/kibo/commands/info.rb
@@ -10,6 +10,11 @@ module Kibo::Commands
       info.line "current account", h.whoami
       info.line "expected account", Kibo.config.account
 
+      info.head "processes"      
+      Kibo.config.processes.each do |key, value|
+        info.line key, value
+      end
+
       info.head "remotes"
       info.line "remotes", h.expected_remotes
       info.line "configured", h.configured_remotes
